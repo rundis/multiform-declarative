bb320a2620f8968a9b89caf9dfef3c2f6760e925
rundis <mrundberget@hotmail.com>
2012-11-24 19:19:39 +0100

    Inheritance, inheritance! integer-range

--- buster.js
+++ buster.js
@@ -2,7 +2,7 @@ var config = module.exports;
 
 config["Browser tests"] = {
   libs: ["external/underscore.js", "external/cull.js", "external/**.js"],
-  sources: ["lib/**/*.js"],
+  sources: ["lib/input-field.js", "lib/**/*.js"],
   tests: ["test/**/*-test.js"],
   environment: "browser"
 };
---
+++ test/integer-range-test.js
@@ -0,0 +1,25 @@
+(function () {
+    buster.testCase("Integer Range", {
+        setUp: function () {
+            this.field = sfk.components.integerRange.create({
+                name: "blapp",
+                min: 5,
+                max: 7
+            });
+
+            this.element = this.field.getElement();
+        },
+
+        "should be a regular old input for now": function () {
+            assert.tagName(this.element, "input");
+        },
+
+        "should be invalid outside of range": function () {
+            this.element.value = 8;
+            refute(this.field.isValid());
+
+            this.element.value = 7;
+            assert(this.field.isValid());
+        }
+    });
+}());
