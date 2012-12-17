a6bebfbbd360dee50b83c123ab157c3de9ece4fb
rundis <mrundberget@hotmail.com>
2012-12-17 23:36:54 +0100

    Refactored behaviour from saveable form to form components object

--- test/form-components-template-test.js
+++ test/form-components-template-test.js
@@ -1,17 +1,49 @@
 (function () {
-    buster.testCase("Form Components Template", {
-        "should create components form template definition": function () {
-            var formComponents = {
+    buster.testCase("Form Components", {
+        setUp: function () {
+            this.formComponents = {
                 "status":      {type: "formStatus"},
-                "individ":     {type: "integerRange", label: "Individ"}
+                "individ":     {type: "inputField", label: "Individ"}
             };
 
-            var template = sfk.components.formComponentsTemplate.create(formComponents);
-            var components = template.createComponents();
+            this.template = sfk.components.formComponentsTemplate.create(this.formComponents);
+            this.components = this.template.createComponents();
 
-            assert.tagName(components.status.getElement(), "span");
-            assert.tagName(components.individ.getElement(), "input");
+        },
 
+        "template should create components from template definition": function () {
+            assert.tagName(this.components.status.getElement(), "span");
+            assert.tagName(this.components.individ.getElement(), "input");
+
+        },
+
+        "get elements for all components ": function () {
+            var elements = this.components.getElements();
+            var size = _.keys(elements).length;
+
+            assert.equals(size, 2);
+            assert.tagName(elements.status, "span");
+        },
+
+        "get components as a list": function () {
+            var list = this.components.toList();
+
+            assert.equals(list.length, 2);
+        },
+
+        "get state of components": function() {
+            this.components.individ.getElement().value = "123";
+            var state = this.components.getState();
+
+            assert.equals(state.individ, "123");
+        },
+
+        "validates all applicable components": function() {
+            this.components.individ.mandatory = true;
+            refute(this.components.allValid());
+
+            this.components.individ.getElement().value = "123";
+            assert(this.components.allValid());
         }
     });
 }());
